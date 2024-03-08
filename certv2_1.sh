81f1df9746828373c83874915d49b80542e605a8
root <guruchalv@gmail.com>
2024-03-08 10:12:46 +0100

    updateions

--- certv2_1.sh
+++ certv2_1.sh
@@ -3,27 +3,28 @@
 # Parent folder
 parent_folder="/opt/cert/proxy-config/certificates"
 
-# Get the most recently modified folders based on modification time (up to 10 folders)
+# Get the most recently modified folder based on modification time
 recent_folder=$(ls -td "$parent_folder"/*/ | head -n 1)
 
-# Iterate through recent folders
-for recent_folder in "${recent_folders[@]}"; do
-    # Iterate through subfolders
-    for subfolder in "$recent_folder"/; do
-        # Check if both certificate and key files exist
-        cert="$subfolder$(basename "$subfolder").crt"
-        key="$subfolder$(basename "$subfolder").key"
+# Check if the recent_folder is not empty
+if [ -n "$recent_folder" ]; then
+    # Extract the basename of the recent folder
+    recent_folder=$(basename "$recent_folder")
 
-        if [ -e "$cert" ] && [ -e "$key" ]; then
-            # Verify the consistency between the certificate and key using SHA256
-            if [ "$(openssl x509 -in "$cert" -pubkey -noout -outform pem | openssl sha256)" == "$(openssl pkey -in "$key" -pubout -outform pem | openssl sha256)" ]; then
-                echo "Certificate and Key for $subfolder are matched."
-            else
-                echo "Error: Certificate and Key for $subfolder do not match."
-            fi
+    # Check if both certificate and key files exist
+    cert="$parent_folder/$recent_folder/$recent_folder.crt"
+    key="$parent_folder/$recent_folder/$recent_folder.key"
+
+    if [ -e "$cert" ] && [ -e "$key" ]; then
+        # Verify the consistency between the certificate and key using SHA256
+        if [ "$(openssl x509 -in "$cert" -pubkey -noout -outform pem | openssl sha256)" == "$(openssl pkey -in "$key" -pubout -outform pem | openssl sha256)" ]; then
+            echo "Certificate and Key for $recent_folder are matched."
         else
-            echo "Error: Certificate or Key files not found for $subfolder."
+            echo "Error: Certificate and Key for $recent_folder do not match."
         fi
-    done
-done
-
+    else
+        echo "Error: Certificate or Key files not found for $recent_folder."
+    fi
+else
+    echo "Error: No recent folder found in $parent_folder."
+fi
